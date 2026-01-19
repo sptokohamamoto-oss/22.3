853875088eb3a64cf4510fd56f37600fc6556885
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-19 10:53:15 +0900

    a

--- ConsoleApp22-3/Program.cs
+++ ConsoleApp22-3/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class Employee
+{
+    public abstract void Work(int hours);
+}
+
+public class FullTimeEmployee : Employee
+{
+    public override void Work(int hours)
+    {
+        Console.WriteLine($"Full-time employee works {hours} hours.");
+    }
+}
+
+public class InternEmployee : Employee
+{
+    private const int MaxHours = 4;
+
+    public override void Work(int hours)
+    {
+        int actualHours = Math.Min(hours, MaxHours);
+        Console.WriteLine($"Intern works {actualHours} hours (requested: {hours}).");
+    }
+}
+
